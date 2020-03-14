938fb95d200aaa052d96ddcad238fd5ea5d68032
Muhammad Kamal <mohammedkamalfergany@yahoo.com>
2020-03-14 02:34:48 +0200

    Add Block Break Item Checks
    Signed-off-by: Muhammad Kamal <mohammedkamalfergany@yahoo.com>

--- src/main/java/com/mohkamfer/silkyspawners/listener/SpawnerBreakListener.java
+++ src/main/java/com/mohkamfer/silkyspawners/listener/SpawnerBreakListener.java
@@ -3,11 +3,16 @@ package com.mohkamfer.silkyspawners.listener;
 import com.mohkamfer.silkyspawners.SilkySpawners;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SpawnerBreakListener implements Listener {
 
@@ -34,6 +39,29 @@ public class SpawnerBreakListener implements Listener {
             return;
         }
 
+        Player player = event.getPlayer();
+        ItemStack itemInHand = player.getItemInHand();
+
+        if (!isSilkPickaxe(itemInHand)) return;
+
         instance.getLogger().info("Spawner broken!");
+    }
+
+    private boolean isSilkPickaxe(ItemStack itemStack) {
+        return isPickaxe(itemStack.getType()) && isSilkTouch(itemStack);
+    }
+
+    private boolean isPickaxe(Material material) {
+        List<Material> pickaxes = Arrays.asList(Material.DIAMOND_PICKAXE,
+                Material.GOLDEN_PICKAXE,
+                Material.IRON_PICKAXE,
+                Material.STONE_PICKAXE,
+                Material.WOODEN_PICKAXE);
+
+        return pickaxes.contains(material);
+    }
+
+    private boolean isSilkTouch(ItemStack itemStack) {
+        return itemStack.containsEnchantment(Enchantment.SILK_TOUCH);
     }
 }
