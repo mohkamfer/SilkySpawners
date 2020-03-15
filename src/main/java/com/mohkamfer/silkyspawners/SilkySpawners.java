2737d924a80d6a72b69a81b4d109e996832e0b3f
Muhammad Kamal <mohammedkamalfergany@yahoo.com>
2020-03-15 03:16:24 +0200

    Drop Spawner On Break
    Signed-off-by: Muhammad Kamal <mohammedkamalfergany@yahoo.com>

--- src/main/java/com/mohkamfer/silkyspawners/SilkySpawners.java
+++ src/main/java/com/mohkamfer/silkyspawners/SilkySpawners.java
@@ -1,20 +1,7 @@
 package com.mohkamfer.silkyspawners;
 
 import com.mohkamfer.silkyspawners.listener.SpawnerBreakListener;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
 
 public final class SilkySpawners extends JavaPlugin {
 
--- src/main/java/com/mohkamfer/silkyspawners/listener/SpawnerBreakListener.java
+++ src/main/java/com/mohkamfer/silkyspawners/listener/SpawnerBreakListener.java
@@ -1,8 +1,14 @@
 package com.mohkamfer.silkyspawners.listener;
 
 import com.mohkamfer.silkyspawners.SilkySpawners;
+import net.minecraft.server.v1_14_R1.NBTTagCompound;
+import net.minecraft.server.v1_14_R1.NBTTagList;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.CreatureSpawner;
+import org.bukkit.craftbukkit.v1_14_R1.inventory.CraftItemStack;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,6 +16,8 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.Arrays;
 import java.util.List;
@@ -44,7 +52,38 @@ public class SpawnerBreakListener implements Listener {
 
         if (!isSilkPickaxe(itemInHand)) return;
 
-        instance.getLogger().info("Spawner broken!");
+        event.setExpToDrop(0);
+
+        BlockState state = block.getState();
+        if (state instanceof CreatureSpawner) {
+            String entity = ((CreatureSpawner) state).getSpawnedType().getKey() + "";
+            instance.getLogger().info(entity);
+            ItemStack drop = new ItemStack(state.getType());
+            ItemMeta itemMeta = drop.getItemMeta();
+            BlockStateMeta blockStateMeta = ((BlockStateMeta) itemMeta);
+            BlockState blockState = blockStateMeta.getBlockState();
+            CreatureSpawner creatureSpawner = (CreatureSpawner) blockState;
+            creatureSpawner.setSpawnedType(((CreatureSpawner) state).getSpawnedType());
+            blockStateMeta.setBlockState(creatureSpawner);
+            itemMeta.setDisplayName(getSpawnerNameFromEntity(entity));
+            drop.setItemMeta(itemMeta);
+            player.getWorld().dropItemNaturally(block.getLocation(), drop);
+        }
+    }
+
+    private String getSpawnerNameFromEntity(String entity) {
+        return capitalizeWords(entity
+                .replace("minecraft:", "")
+                .replace("_", " ") + " spawner");
+    }
+
+    private String capitalizeWords(String s) {
+        String[] s2 = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (String s1 : s2)
+            result.append(Character.toTitleCase(s1.charAt(0))).append(s1.substring(1)).append(' ');
+
+        return result.toString();
     }
 
     private boolean isSilkPickaxe(ItemStack itemStack) {
